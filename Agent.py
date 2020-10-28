fdc7632cce42a8d4a00341b892f7078f16e79fa1
Ellie Talius <64281299+etalius@users.noreply.github.com>
2020-10-28 11:47:43 -0700

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -1,99 +1,115 @@
+from Agent import Agent
+import pygame
 from snake import Snake
+from pygame.locals import *
+import random
 import numpy as np
 
+pygame.init()
+width = 500
+height = 500
+#screen = pygame.display.set_mode((width, height))
 
+
+draw_apple = True
+apple_eaten = False
+
+NUM_SNAKES = 1
 CELL_SIZE = 50
 
-class Agent():
-    def __init__(self, snake, states, actions, epsilon, gamma, alpha, Q):
-        self.snake = snake
-        self.grid_dims = snake.ENV_HEIGHT / CELL_SIZE
-        self.epsilon = epsilon
-        self.Q = Q
-        self.s = 0
-        self.r = 0
-        self.a = 0
-        self.s_prime = 0
-        self.gamma = gamma
-        self.alpha = alpha
+FRAMES_PER_TURN = 600
 
-    def make_state(self):
-        old = self.snake.getState()
-        dir_num = self.dir_to_num(old[2])
-        quad_num = self.apple_quad(old[1], old[0][0])
-        obs = self.get_obstacles(old[0][0])
-        state_list = [dir_num] + [quad_num] + obs
-        state_tup = tuple(state_list)
-        num = np.ravel_multi_index(state_tup, (4, 4, 2, 2, 2, 2))
-        return num
+BACKGROUND_COL = (0, 0, 255)
+SNAKE_COL = (0, 255, 0)
+APPLE_COL = (255, 0, 0)
 
-    def get_obstacles(self, head):
-        obs = [0, 0, 0, 0] #list of if obstacles are present up, right, left, down
-        if head[1] == 0:
-            obs[0] = 1
-        if head[0] == CELL_SIZE:
-            obs[1] = 1
-        if head[1] == self.snake.ENV_HEIGHT - CELL_SIZE:
-            obs[2] = 1
-        if head[0] == self.snake.ENV_WIDTH:
-            obs[3] = 1
-        new_obs = self.check_body(head, obs)
-        return new_obs
+states = 256
+actions = 4
+epsilon = 0.5
+gamma = 0.9
+alpha = 0.2
 
-    def check_body(self, head, obs):
-        for i in range(1, len(self.snake.body)):
-            snek = self.snake.body[i]
-            if head[1] == snek[1] + CELL_SIZE:
-                obs[0] = 1
-            if head[0] == snek[0] + CELL_SIZE:
-                obs[1] = 1
-            if head[1] == snek[1] - CELL_SIZE:
-                obs[2] = 1
-            if head[0] == snek[0] - CELL_SIZE:
-                obs[3] = 1
-        return obs
+Q = np.zeros((states, actions))
+
+#snake = Snake(width, height)
+#agent = Agent(snake, states, actions, epsilon, gamma, alpha, Q)
+#agent.s = agent.make_state()
+num_episodes = 100
+directions = ["UP", "DOWN", "LEFT", "RIGHT"]
+
+for i in range(num_episodes):
+    snake = Snake(width, height)
+    agent = Agent(snake, states, actions, epsilon, gamma, alpha, Q)
+    agent.s = agent.make_state()
+
+    play_game = True
+    frames_passed = 0
+    dir_updated = False
+    score = 0
 
 
-    def apple_quad(self, apple, head):
-         if apple[0] < head[0] and apple[1] < head[1]:
-             #top right
-             return 0
-         if apple[0] >= head[0] and apple[1] < head[1]:
-             #top left
-             return 1
-         if apple[0] >= head[0] and apple[1] >= head[1]:
-             #bottom left
-             return 2
-         else:
-             #bottom right
-             return 3
+    while play_game:
+        #screen.fill(BACKGROUND_COL)
 
-    def dir_to_num(self, dir):
-        if dir == "UP":
-            return 0
-        if dir == "RIGHT":
-            return 1
-        if dir == "DOWN":
-            return 2
-        if dir == "LEFT":
-                return 3
+        if not dir_updated:
+            if np.random.rand() < agent.epsilon:
+               action = random.choice(directions)
+               snake.updateDirection(action)
+               agent.a = agent.dir_to_num(action)
+            else:
+               options = agent.Q[agent.s]
+               act_num = np.argmax(options)
+               agent.a = act_num
+               action = agent.dir_to_num(act_num)
+               snake.updateDirection(action)
+            dir_updated = True
 
-    def num_to_dir(self, num):
-        if num == 0:
-            return "UP"
-        if num == 1:
-            return "RIGHT"
-        if num == 2:
-            return "DOWN"
-        if num == 3:
-            return "LEFT"
 
-    def updateQ(self):
-        gamma, Q, alpha = self.gamma, self.Q, self.alpha
-        s_prime_row = self.Q[self.s_prime]
-        max_new_Q = s_prime_row.max()
-        Q[self.s, self.a] += alpha * (self.r + gamma * max_new_Q - Q[self.s, self.a])
+        #sets the apple location
+        if draw_apple:
+            draw_apple = False
+            snake.setNewAppleLocation()
+        #draws apple location
+        #pygame.draw.rect(screen, APPLE_COL, (snake.apple[0], snake.apple[1], CELL_SIZE, CELL_SIZE))
 
+        #checks to see if the snake head is on the apple aka eats the apple
+        if snake.getHeadLocation() == snake.apple:
+            draw_apple = True
+            apple_eaten = True
+            score += 1
+
+        #dictates the speed of the game
+        if frames_passed > FRAMES_PER_TURN:
+            dir_updated = False
+            if not snake.isAlive():
+                play_game = False
+                agent.r = -1000
+            frames_passed = 0
+            snake.updateBody(apple_eaten)
+            agent.s_prime = agent.make_state()
+            if apple_eaten:
+                agent.r = 100
+            else:
+                agent.r = -0.1
+            apple_eaten = False
+
+        #draws the snake at every time step
+        for snek in snake.body:
+            pass
+            #pygame.draw.rect(screen, SNAKE_COL, (snek[0], snek[1], CELL_SIZE, CELL_SIZE))
+            #pygame.draw.rect(screen, SNAKE_COL, (snek[0] + 1, snek[1] + 1, CELL_SIZE - 2, CELL_SIZE - 2))
+
+        agent.updateQ()
+
+        #pygame.display.update()
+        frames_passed += 1
+
+    print(f"You got {score} points!")
+
+##need to add way to save policy and re-use
+
+print(Q)
+pygame.quit()
 
 
 
